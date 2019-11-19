129b113543a785bbe56f6d5f9c90a366df7d2318
Konstantin Iroshnikov <k.iroshnikov@itmegastar.com>
2019-11-19 15:52:00 +0700

    добавление данных

---
+++ videoVC/PlayerViewController.swift
@@ -0,0 +1,41 @@
+//
+//  PlayerViewController.swift
+//  GinzaGO
+//
+//  Created by Username on 31.07.2019.
+//  Copyright © 2019 ITMegastar. All rights reserved.
+//
+
+import UIKit
+import AVKit
+
+class PlayerViewController: AVPlayerViewController {
+    var urlVideo: URL?
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        playVideo()
+    }
+
+    static func route(url: URL?) -> PlayerViewController? {
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        let viewController = storyboard.instantiateViewController(withIdentifier: "PlayerViewController")
+        let controller = viewController as? PlayerViewController
+
+        controller?.urlVideo = url
+
+        return controller
+    }
+
+    private func playVideo() {
+        if let url = self.urlVideo {
+            let player = AVPlayer(url: url)
+            let playerLayer = AVPlayerLayer(player: player)
+            playerLayer.frame = self.view.bounds
+            self.view.layer.addSublayer(playerLayer)
+            self.player = player
+            player.play()
+        }
+    }
+}
