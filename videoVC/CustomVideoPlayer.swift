c35c831b297af9bde6c3e3efdab8da74299a8b9f
Konstantin Iroshnikov <k.iroshnikov@itmegastar.com>
2019-11-20 13:10:29 +0700

    анимация появления/исчезновения прогресс вью

--- videoVC/CustomVideoPlayer.swift
+++ videoVC/CustomVideoPlayer.swift
@@ -17,28 +17,81 @@ class CustomVideoPlayer: UIViewController {
     @IBOutlet weak var bacgroundView: UIView!
 
 
+    @IBOutlet weak var volumeView: UIProgressView!
+    let audioSession = AVAudioSession.sharedInstance()
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
-//        addVolumeProgressView()
+//        self.audioSession.outputVolume
+//        print(startValue)
 
-//        let  audioSession = AVAudioSession.sharedInstance()
-//        let volume : Float = audioSession.outputVolume
-//
-//        print(volume)
+//        let volumeView = MPVolumeView(frame: CGRect(x: 0, y: 0, width: 10, height: 10))
+//        volumeView.isHidden = true
+//        view.addSubview(volumeView)
 
-        let volumeView = MPVolumeView(frame: CGRect(x: 100, y: 100, width: 100, height: 100))
+        addClearSlider()
 
-        volumeView.isHidden = false
-        volumeView.alpha = 1
-//
-        view.addSubview(volumeView)
+    }
 
+    override func viewWillDisappear(_ animated: Bool) {
+        audioSession.removeObserver(self, forKeyPath: "outputVolume")
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        listenVolumeButton()
+    }
+
+    //MARK: - Volume
+
+
+    private func addClearSlider(){
+        let volumeViewClear = MPVolumeView(frame: CGRect(x: 0, y: 0, width: 10, height: 10))
+        view.addSubview(volumeViewClear)
+
+        volumeView.setProgress(self.audioSession.outputVolume, animated: false)
+
+        colorProgressView(true)
+    }
+
+    private func colorProgressView(_ clear: Bool){
+
+        let progress = clear ? UIColor.clear : UIColor.white
+        let allColor = clear ? UIColor.clear : UIColor(red: 200/255, green: 200/255, blue: 200/255, alpha: 1)
+
+        volumeView.progressTintColor = progress
+        volumeView.trackTintColor = allColor
+    }
+
+    private func listenVolumeButton(){
+
+        do{
+            try audioSession.setActive(true)
+            let vol = audioSession.outputVolume
+            print(vol.description) //gets initial volume
+        }
+        catch{
+            print("Error info: \(error)")
+        }
+        audioSession.addObserver(self, forKeyPath: "outputVolume", options:
+            NSKeyValueObservingOptions.new, context: nil)
+    }
+
+    override func observeValue(forKeyPath keyPath: String?, of object: Any?, change: [NSKeyValueChangeKey : Any]?, context: UnsafeMutableRawPointer?) {
+        if keyPath == "outputVolume"{
+            let volume = (change?[NSKeyValueChangeKey.newKey] as! NSNumber).floatValue
+
+            let valueVolume = Float(volume.description) ?? self.audioSession.outputVolume
+            volumeView.setProgress(valueVolume, animated: true)
+        }
     }
 
 
 
+
+
     deinit {
+        NotificationCenter.default.removeObserver(self)
         print("deinit")
     }
 
